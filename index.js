55bba6f4170449f9debdca698b9aae4ee154ebd4
omkarkokil <nsjustice21@gmail.com>
2024-09-27 08:27:41 +0530

    now it works

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world " + process.env.test);
+app.get("/api/", (req, res) => {
+  res.send("users data" + process.env.test);
+});
+
+app.get("/api/users", (req, res) => {
+  const users = {
+    user1: "First User",
+    user2: "Test  User",
+    user2: "Test  User",
+    user2: "Test  User",
+  };
+  res.status(200).json(users);
 });
 
 app.listen(5000, () => {
